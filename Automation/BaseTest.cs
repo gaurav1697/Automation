1a04f8c441a419520b57158ad355129f28457662
gaurav1697 <gaurav.kachwaha16@gmail.com>
2025-04-02 15:57:42 +0530

    initial commit

---
+++ Automation/BaseTest.cs
@@ -0,0 +1,30 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace Automation
+{
+    namespace SeleniumAutomation
+    {
+        public class BaseTest
+        {
+            protected IWebDriver driver;
+
+            [SetUp]
+            public void SetUp()
+            {
+                driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://magento.softwaretestingboard.com");
+            }
+
+            [TearDown]
+            public void TearDown()
+            {
+                driver.Quit();
+            }
+        }
+    }
+
+}
