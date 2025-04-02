7f7ca7b0dd6e722b2fc09e27f9ce4df4cdf6d66c
gaurav1697 <gaurav.kachwaha16@gmail.com>
2025-04-02 20:16:27 +0530

    modifiedemail

--- Automation/Test.cs
+++ Automation/Test.cs
@@ -15,7 +15,7 @@ namespace Automation
             driver.FindElement(createAccountPage.CreateButton).Click();
             driver.FindElement(createAccountPage.FirstName).SendKeys("Test Username");
             driver.FindElement(createAccountPage.LastName).SendKeys("Last Name");
-            driver.FindElement(createAccountPage.Email).SendKeys("pelelo5234@deenur.com");
+            driver.FindElement(createAccountPage.Email).SendKeys("yy65u@ptct.net");
             driver.FindElement(createAccountPage.Password).SendKeys("Password123");
             driver.FindElement(createAccountPage.PasswordConfirm).SendKeys("Password123");
             driver.FindElement(createAccountPage.CreateAnAccountButton).Click();
