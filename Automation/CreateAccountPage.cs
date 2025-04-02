ac9ab0ca977684ab95e41707da14f6c50b723de9
gaurav1697 <gaurav.kachwaha16@gmail.com>
2025-04-02 16:03:09 +0530

    Automation Assesment

---
+++ Automation/CreateAccountPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.BiDi.Modules.BrowsingContext;
+
+namespace Automation
+{
+    public class CreateAccountPage
+    {
+
+        public By CreateButton = By.XPath("(//li/a[normalize-space(text())='Create an Account'])[1]");
+        public By FirstName = By.XPath("[id = firstname]");
+        public By LastName = By.XPath("[id = lastname]");
+        public By Email = By.XPath("[id = email_address]");
+        public By Password = By.XPath("[id = password]");
+        public By PasswordConfirm = By.XPath("[id = password-confirmation]");
+        public By CreateAnAccountButton = By.XPath("//button[@title = 'Create an Account']");
+
+    }
+}
---
+++ Automation/Test.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using NUnit.Framework;
+using Automation.SeleniumAutomation;
+
+namespace Automation
+{
+    public class Test : BaseTest
+    {
+        [Test]
+        public void GoogleSearch()
+        {
+            CreateAccountPage createAccountPage = new CreateAccountPage();
+
+            
+            driver.FindElement(createAccountPage.CreateButton).Click();
+            driver.FindElement(createAccountPage.FirstName).SendKeys("First Name");
+            driver.FindElement(createAccountPage.LastName).SendKeys("Last Name");
+            driver.FindElement(createAccountPage.FirstName).SendKeys("First Name");
+            driver.FindElement(createAccountPage.FirstName).SendKeys("First Name");
+
+
+
+
+            //Assert.IsTrue(driver.Title.Contains("Selenium C# tutorial"));
+        }
+    }
+}
